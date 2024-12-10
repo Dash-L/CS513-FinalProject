6cda8ca0f4cf707c84e46243344c8d6209d399c0
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-12-09 22:26:49 -0500

    thought I fixed a memory bug, but now there is one I can't figure out...

--- main.c
+++ main.c
@@ -8,31 +8,31 @@ typedef struct {
 
 } ROUTER_INFO;
 
-#define VEC(typ)                                                               \
+#define VEC(T)                                                                 \
   typedef struct {                                                             \
-    typ *start;                                                                \
+    T *start;                                                                  \
     size_t size;                                                               \
     size_t cap;                                                                \
-  } typ##_VEC;                                                                 \
-  typ##_VEC *typ##_VEC_create() {                                              \
-    typ##_VEC *vec = malloc(sizeof(*vec));                                     \
+  } T##_VEC;                                                                   \
+  T##_VEC *T##_VEC_create() {                                                  \
+    T##_VEC *vec = malloc(sizeof(*vec));                                       \
     vec->cap = 8;                                                              \
-    vec->start = malloc(sizeof(typ) * vec->cap);                               \
+    vec->start = malloc(sizeof(T) * vec->cap);                                 \
     vec->size = 0;                                                             \
     return vec;                                                                \
   }                                                                            \
-  void typ##_VEC_append(typ##_VEC *vec, typ elem) {                            \
+  void T##_VEC_append(T##_VEC *vec, T elem) {                                  \
     if (vec->size >= vec->cap) {                                               \
       vec->cap *= 2;                                                           \
-      vec->start = realloc(vec->start, vec->cap);                              \
+      vec->start = realloc(vec->start, sizeof(T) * vec->cap);                  \
     }                                                                          \
     vec->start[vec->size++] = elem;                                            \
   }                                                                            \
-  void typ##_VEC_remove(typ##_VEC *vec, size_t idx) {                          \
+  void T##_VEC_remove(T##_VEC *vec, size_t idx) {                              \
     memmove(vec->start + idx, vec->start + idx + 1, vec->size - idx - 1);      \
     vec->size -= 1;                                                            \
   }                                                                            \
-  typ typ##_VEC_pop(typ##_VEC *vec) { return vec->start[--vec->size]; }
+  T T##_VEC_pop(T##_VEC *vec) { return vec->start[--vec->size]; }
 
 struct NODE_INFO;
 
@@ -65,45 +65,45 @@ VEC(NODE_INFO);
 void *router(void *args) {}
 
 // Threadsafe queue
-#define QUEUE(typ)                                                             \
+#define QUEUE(T)                                                               \
   typedef struct {                                                             \
-    typ##_VEC *vec;                                                            \
+    T##_VEC *vec;                                                              \
     pthread_mutex_t mutex;                                                     \
     pthread_cond_t nonempty;                                                   \
-  } typ##_QUEUE;                                                               \
+  } T##_QUEUE;                                                                 \
                                                                                \
-  typ##_QUEUE *typ##_QUEUE_create() {                                          \
-    typ##_QUEUE *queue = malloc(sizeof(*queue));                               \
-    typ##_VEC *vec = typ##_VEC_create();                                       \
+  T##_QUEUE *T##_QUEUE_create() {                                              \
+    T##_QUEUE *queue = malloc(sizeof(*queue));                                 \
+    T##_VEC *vec = T##_VEC_create();                                           \
     queue->vec = vec;                                                          \
     pthread_mutex_init(&queue->mutex, NULL);                                   \
     pthread_cond_init(&queue->nonempty, NULL);                                 \
     return queue;                                                              \
   }                                                                            \
-  void typ##_QUEUE_push(typ##_QUEUE *queue, typ elem) {                        \
+  void T##_QUEUE_push(T##_QUEUE *queue, T elem) {                              \
     pthread_mutex_lock(&queue->mutex);                                         \
-    typ##_VEC_append(queue->vec, elem);                                        \
+    T##_VEC_append(queue->vec, elem);                                          \
     pthread_cond_signal(&queue->nonempty);                                     \
     pthread_mutex_unlock(&queue->mutex);                                       \
   }                                                                            \
   /* Guaranteed to pop an element, might wait a while for one to be there */   \
-  void typ##_QUEUE_pop(typ##_QUEUE *queue, typ *out) {                         \
+  void T##_QUEUE_pop(T##_QUEUE *queue, T *out) {                               \
     pthread_mutex_lock(&queue->mutex);                                         \
     while (queue->vec->size == 0) {                                            \
       pthread_cond_wait(&queue->nonempty);                                     \
     }                                                                          \
-    *out = typ##_VEC_pop(queue->vec);                                          \
+    *out = T##_VEC_pop(queue->vec);                                            \
     pthread_mutex_unlock(&queue->mutex);                                       \
   }                                                                            \
   /* Returns -1 if there is nothing to pop. Still waits on mutex until it      \
    * unlocks */                                                                \
-  int typ##_QUEUE_trypop(typ##_QUEUE *queue, typ *out) {                       \
+  int T##_QUEUE_trypop(T##_QUEUE *queue, T *out) {                             \
     if (queue->vec->size == 0)                                                 \
       return -1;                                                               \
     pthread_mutex_lock(&queue->mutex);                                         \
     if (queue->vec->size == 0)                                                 \
       return -1;                                                               \
-    *out = typ##_VEC_pop(queue->vec);                                          \
+    *out = T##_VEC_pop(queue->vec);                                            \
     pthread_mutex_unlock(&queue->mutex);                                       \
     return 0;                                                                  \
   }
@@ -117,7 +117,9 @@ int main(int argc, char **argv) {
   double cost;
 
   int res;
-  while ((res = scanf(" %c %c %s", &node1, &node2, cost_input)) == 3) {
+  while ((res = scanf(" %c %c %63[^\n]", &node1, &node2, cost_input)) == 3) {
+    // printf("input = { node1 = %c, node2 = %c, cost = %s }\n", node1, node2,
+    // cost_input);
     ssize_t node1_idx = -1, node2_idx = -1;
     for (int i = 0; i < nodes->size; i++) {
       if (nodes->start[i].name == node1)
@@ -199,6 +201,7 @@ int main(int argc, char **argv) {
           .c = cost,
       };
       EDGE_VEC_append(node1_edges, new_edge);
+      // printf("New edge to %c from %c with cost %f\n", node2, node1, cost);
     }
     if (!node2_edge_found) {
       EDGE new_edge = {
@@ -207,7 +210,18 @@ int main(int argc, char **argv) {
           .c = cost,
       };
       EDGE_VEC_append(node2_edges, new_edge);
+      // printf("New edge to %c from %c with cost %f\n", node1, node2, cost);
     }
+    printf("Nodes: %zu\n", nodes->size);
+    for (int i = 0; i < nodes->size; i++) {
+      printf("%c\n", nodes->start[i].name);
+      for (int j = 0; j < nodes->start[i].edges->size; j++) {
+        printf("  -> %c %.0f\n", nodes->start[i].edges->start[j].b->name,
+               nodes->start[i].edges->start[j].c);
+      }
+      printf("\n");
+    }
+    printf("\n\n");
   }
 
   printf("Nodes: %zu\n", nodes->size);
@@ -219,6 +233,7 @@ int main(int argc, char **argv) {
     }
     printf("\n");
   }
+  printf("\n\n");
 
   return 0;
 }
